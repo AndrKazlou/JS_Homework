38f8d8af1ecb9e89f488a32fd688b2ab228b23b0
akazlou <akazlou@macbun.local>
2024-02-04 15:29:13 +0300

    Learn data types

---
+++ HW_3/delenieTipov.js
@@ -0,0 +1,11 @@
+console.log('qwe' / true);
+
+console.log('qwe' / 123);
+
+console.log(123 / true);
+
+console.log(132/ false);
+
+console.log(false / false);
+
+console.log(0 / 0);
---
+++ HW_3/preobrazovanieTipov.js
@@ -0,0 +1,42 @@
+let str = "123aa123"
+
+let num = 123
+
+let bool = true
+
+const x1 = Number (str);
+console.log(x1);
+console.log(typeof x1);
+
+const x2 = Number (num);
+console.log(x2);
+console.log(typeof x2);
+
+const x3 = Number (bool);
+console.log(x3);
+console.log(typeof x3);
+
+const x4 = String (str);
+console.log(x4);
+console.log(typeof x4);
+
+const x5 = String (num);
+console.log(x5);
+console.log(typeof x5);
+
+const x6 = String (bool);
+console.log(x6);
+console.log(typeof x6);
+
+
+const x7 = Boolean (str);
+console.log(x7);
+console.log(typeof x7);
+
+const x8 = Boolean (num);
+console.log(x8);
+console.log(typeof x8);
+
+const x9 = Boolean (bool);
+console.log(x9);
+console.log(typeof x9);
